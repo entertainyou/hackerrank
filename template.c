1eeeb58c642cdf074bb62d6e5c77dbeb3b9d4172
HUANG Wei <weih@opera.com>
2014-04-22 21:35:53 +0800

    Update template.c

--- template.c
+++ template.c
@@ -3,6 +3,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+static int read_int() {
+    int ret;
+    scanf("%d", &ret);
+    return ret;
+}
+
+/* caller should free the memory */
+static int *read_int_array(int n) {
+    int *ret = malloc(n * sizeof(int));
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", ret + i);
+    }
+    return ret;
+}
+
+static int intcomp(const void *v1, const void *v2) {
+    return *(const int *)v1 - *(const int *)v2;
+}
+
 int main(int argc, char *argv[])
 {
     
