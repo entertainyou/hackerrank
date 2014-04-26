71a0ba15ed9c9d4edb85f3efed1058708020451e
HUANG Wei <weih@opera.com>
2014-04-26 17:00:56 +0800

    WIP

--- challenges/w1/bst_maintenance.c
+++ challenges/w1/bst_maintenance.c
@@ -24,7 +24,7 @@ static int intcomp(const void *v1, const void *v2) {
 
 struct node_info {
     int left_count, right_count, top_count;
-    int left_weight, right_weight, top_weight;
+    long long int left_weight, right_weight, top_weight;
     int val;
 };
 
@@ -95,7 +95,8 @@ static void bst_print_internal(node *parent) {
     if (parent->left) {
         bst_print_internal(parent->left);
     }
-    printf("%d ", data->val);
+    printf("%d(%d,%d,%d  %lld,%lld,%lld) ", data->val, data->left_count, data->right_count, data->top_count,
+           data->left_weight, data->right_weight, data->top_weight);
     if (parent->right) {
         bst_print_internal(parent->right);
     }
@@ -116,43 +117,132 @@ static void bst_print() {
 static void bst_finish() {
 }
 
-/* 0 is parent, 1 is child left, 2 is child right */
-static void bst_update(node *node, int origin) {
-    if (node == NULL) {
+/* 0 is parent, 1 is left child, 2 is right child */
+static void bst_update(node *current, int origin) {
+    if (current == NULL) {
         return;
     }
 
-    if (origin == 0) {
-        node *parent = node->parent;
-        struct node_info *current_node_info = parent->data;
-        assert(current_node_info != NULL);
-        struct node_info *parent_node_info = parent->data;
-        assert(parent_node_info != NULL);
-        current_node_info->top_count = 1 + parent_node_info->top_count;
-        current_node_info->top_weight = 1 + parent_node_info->top_count + parent_node_info->top_weight;
+    node *other_node;
 
-        if (node == parent->left) {
-            current_node_info->top_count += parent_node_info->right_count;
-            current_node_info->top_weight += parent_node_info->right_count + parent_node_info->right_weight;
+    if (origin == 0) {
+        other_node = current->parent;
+    } else if (origin == 1) {
+        other_node = current->left;
+    } else if (origin == 2) {
+        other_node = current->right;
+    } else {
+        assert(0);
+    }
+
+    struct node_info *current_node_info = current->data;
+    assert(current_node_info != NULL);
+    struct node_info *other_node_info = other_node->data;
+    assert(other_node_info != NULL);
+
+    /* printf("bst_update %d origin: %d\n", current_node_info->val, origin); */
+    if (origin == 0) {
+        current_node_info->top_count = 1 + other_node_info->top_count;
+        current_node_info->top_weight = 1 + other_node_info->top_count + other_node_info->top_weight;
+
+        if (current == other_node->left) {
+            current_node_info->top_count += other_node_info->right_count;
+            current_node_info->top_weight += other_node_info->right_count + other_node_info->right_weight;
         } else {
-            assert(node == parent->right);
-            current_node_info->top_count += parent_node_info->left_count;
-            current_node_info->top_weight += parent_node_info->left_count + parent_node_info->left_weight;
+            assert(current == other_node->right);
+            current_node_info->top_count += other_node_info->left_count;
+            current_node_info->top_weight += other_node_info->left_count + other_node_info->left_weight;
         }
 
-        bst_update(node->left, 0);
-        bst_update(node->right, 0);
-    } 
+        current_node_info->top_weight %= 1000000007;
+        bst_update(current->left, 0);
+        bst_update(current->right, 0);
+    } else if (origin == 1) {
+        current_node_info->left_count = 1 + other_node_info->left_count;
+        current_node_info->left_weight = 1 + other_node_info->left_count + other_node_info->left_weight;
+
+        current_node_info->left_count += other_node_info->right_count;
+        current_node_info->left_weight += other_node_info->right_count + other_node_info->right_weight;
+
+        current_node_info->top_weight %= 1000000007;
+        if (current->parent) {
+            bst_update(current->parent, current->parent->left == current ? 1 : 2);
+        }
+        bst_update(current->right, 0);
+    } else if (origin == 2) {
+        current_node_info->right_count = 1 + other_node_info->left_count;
+        current_node_info->right_weight = 1 + other_node_info->left_count + other_node_info->left_weight;
+
+        /* printf("A\n"); */
+        /* bst_print(); */
+        /* printf("B\n"); */
+        current_node_info->right_count += other_node_info->right_count;
+        current_node_info->right_weight += other_node_info->right_count + other_node_info->right_weight;
+
+        current_node_info->top_weight %= 1000000007;
+        /* printf("C\n"); */
+        /* bst_print(); */
+        /* printf("D\n"); */
+
+        if (current->parent) {
+            bst_update(current->parent, current->parent->left == current ? 1 : 2);
+        }
+        bst_update(current->left, 0);
+    }
+
+    /* current_node_info->top_count = 1 + parent_node_info->top_count; */
+    /* current_node_info->top_weight = 1 + parent_node_info->top_count + parent_node_info->top_weight; */
+
+    /* if (node == parent->left) { */
+    /*     current_node_info->top_count += parent_node_info->right_count; */
+    /*     current_node_info->top_weight += parent_node_info->right_count + parent_node_info->right_weight; */
+    /* } else { */
+    /*     assert(node == parent->right); */
+    /*     current_node_info->top_count += parent_node_info->left_count; */
+    /*     current_node_info->top_weight += parent_node_info->left_count + parent_node_info->left_weight; */
+    /* } */
+
+    /* bst_update(node->left, 0); */
+    /* bst_update(node->right, 0); */
 }
 
 int main(int argc, char *argv[]) {
     int n = read_int();
     struct node_info *node_array = calloc(n, sizeof(struct node_info));
     bst_init(node_comp);
+    long long answer = 0;
     for (int i = 0; i < n; ++i) {
         node_array[i].val = read_int();
-        node * _node = bst_feed(node_array + i);
-        bst_print();
+        node *_node = bst_feed(node_array + i);
+        node *parent = _node->parent;
+        struct node_info *current_info = _node->data;
+        /* printf("ALICE\n"); */
+        /* bst_print(); */
+        /* update _node info */
+        if (parent != NULL) {
+            struct node_info *parent_info = parent->data;
+            current_info->top_count = parent_info->top_count + 1;
+            /* printf("BOB\n"); */
+            /* bst_print(); */
+            /* printf("%d %d %d\n", parent_info->top_count, 1, parent_info->top_weight); */
+            current_info->top_weight = parent_info->top_count + 1 + parent_info->top_weight;
+            if (_node == parent->left) {
+                current_info->top_count += parent_info->right_count;
+                current_info->top_weight += parent_info->right_weight + parent_info->right_count;
+            } else {
+                current_info->top_count += parent_info->left_count;
+                current_info->top_weight += parent_info->left_weight + parent_info->left_count;
+            }
+
+            bst_update(parent, _node == parent->left ? 1 : 2);
+        }
+        /* bst_print(); */
+        answer += (current_info->top_weight);
+        answer %= 1000000007;
+
+        /* assert(answer % 2 == 0); */
+        printf("%lld\n", answer);
+        /* bst_print(); */
     }
 
     bst_finish();
